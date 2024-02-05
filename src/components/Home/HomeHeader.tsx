d4a76cf6e9ed2e41efbe6d0c710292f84e24f1aa
Tomas Leggire <tomasleggire@hotmail.com>
2024-02-05 16:03:13 -0300

    Cambio a docs

--- src/components/Home/HomeHeader.tsx
+++ src/components/Home/HomeHeader.tsx
@@ -1,19 +1,10 @@
 import { StyleSheet, View, Text, Pressable } from "react-native";
 import React from "react";
 
-interface Props {
-  openModal: () => void;
-}
-
-export default function HomeHeader(props: Props) {
-  const { openModal } = props;
-
+export default function HomeHeader() {
   return (
     <View style={styles.headerContent}>
-      <Text style={styles.title}>¿Qué tenes a mano?</Text>
-      <Pressable style={styles.btnIngredientes} onPress={openModal}>
-        <Text>INGREDIENTES</Text>
-      </Pressable>
+      <Text style={styles.title}>Camara</Text>
     </View>
   );
 }
@@ -27,12 +18,5 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: "bold",
     fontSize: 28,
-  },
-  btnIngredientes: {
-    marginTop: 30,
-    backgroundColor: "pink",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 15,
   },
 });
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,24 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HomeHeader from "../components/Home/HomeHeader";
-import IngredientsModal from "../components/Home/IngredientsModal";
 
 export default function Home() {
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const openModal = () => {
-    setModalVisible(true);
-  };
-
-  const closeModal = () => {
-    setModalVisible(false);
-  };
-
   return (
     <SafeAreaView style={styles.homeContent}>
-      <HomeHeader openModal={openModal} />
-      <IngredientsModal modalVisible={modalVisible} closeModal={closeModal} />
+      <HomeHeader />
     </SafeAreaView>
   );
 }
